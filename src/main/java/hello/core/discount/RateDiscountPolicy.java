e7abf6aa65e5fcfa30acab9da74e4a87e2078388
Soomin Kim <klynn0403@gmail.com>
2022-03-15 00:44:01 +0900

    2022.03.14 [커스텀 애노테이션]

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,12 +1,12 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
     private int disCountFixAmount = 10; //10% 할인
     @Override
