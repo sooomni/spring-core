5b7e8a1e939b3361c65e18b4c85ba3a811f582aa
Soomin Kim <klynn0403@gmail.com>
2022-03-15 00:39:14 +0900

    2022.03.14 [조회 빈이 2개 이상일 때 @Qualifier, @Primary]

--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -26,7 +26,7 @@ public class ApplicationContextSameBeanFindTest {
     @Test
     @DisplayName("타입으로 조회 시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
     void findBeanByName() {
-        MemberRepository memberRepository = ac.getBean("memberRepository1", MemberRepository.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
         assertThat(memberRepository).isInstanceOf(MemberRepository.class);
     }
 
