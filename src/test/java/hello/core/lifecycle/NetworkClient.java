e6e7ff2f55e4a06fb57c40e9bf70fab977e864c5
Soomin Kim <klynn0403@gmail.com>
2022-03-17 01:56:25 +0900

    2022.03.17 [빈 생명주기 콜백 - @PostConstruct, @PreDestroy]

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,37 @@
+package hello.core.lifecycle;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient
+//implements InitializingBean, DisposableBean
+{
+    private String url;
+
+
+    public NetworkClient(){
+        System.out.println("constructor 호출, url : "+url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public void connect(){
+        System.out.println("connect url : "+url);
+    }
+    public void call(String msg){
+        System.out.println("call : "+url+" messege : "+msg);
+    }
+    public void disconnect(){
+        System.out.println("close : "+url);
+    }
+    @PostConstruct
+    public void init() throws Exception {
+        connect();
+        call("초기화 연결");
+    }
+    @PreDestroyg
+    public void close() throws Exception {
+        disconnect();
+    }
+}
