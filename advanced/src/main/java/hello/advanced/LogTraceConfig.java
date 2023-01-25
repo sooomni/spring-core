cc0665440e64c099d686527ec0bde0c32ac672de
Soomin Kim <klynn0403@gmail.com>
2023-01-26 01:41:32 +0900

    2023.01.24 [ThreadLocal 적용]

--- advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package hello.advanced;
 
-import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +10,6 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace(){
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
---
+++ advanced/src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,43 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+    private ThreadLocalService threadLocalService = new ThreadLocalService();
+
+    @Test
+    void field(){
+        log.info("main start");
+        Runnable userA = () -> {
+            threadLocalService.logic("userA");
+        };
+
+        Runnable userB= () -> {
+            threadLocalService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(100); // 동시성 문제 발생 X;
+        threadB.start();
+
+        sleep(3000); // 메인스레드 종료 대기
+        log.info("main thread exit");
+    }
+
+    private void sleep(int millis) {
+        try{
+            Thread.sleep(millis);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
